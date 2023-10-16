889d7b60346d726264c9a422aee9a487581edaf1
Kennedy <854543+kennedy@users.noreply.github.com>
2023-10-16 00:49:31 -0400

    potentially swapping the info flow

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -16,17 +16,18 @@ export default async function Address({ params }: Props) {
   return (
     <main>
       <div>
+        <Info params={params} />
         <h1>testing</h1>
         <h2>address: {params.slug}</h2>
         <p>{data.response.message}</p>
         <code>{JSON.stringify(data.response)}</code>
       </div>
-      <Info params={params}/>
     </main>
   );
 }
 
 async function getData(address: string) {
+  const res = await fetch(`https://api.omg.lol/address/${address}/info`, { cache: "force-cache" });
   const res = await fetch(`https://api.omg.lol/address/${address}/availability`, { cache: "force-cache" });
   if (!res.ok) {
     throw new Error("Failed to fetch data!");
